bb42a9cd91ffd586fac590f6d58ab406d2b53dec
akashRbn <akashlog402@gmail.com>
2025-11-11 17:58:51 +0600

    add static favicon

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8" />
     <title>@yield('title', 'Auth - Glodex')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('back-end/assets/images/glodex-favicon.png')}}">
     <link href="{{ asset('back-end/assets/css/vendor.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('back-end/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
     <link href="{{ asset('back-end/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
